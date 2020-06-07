556932494bd53278cc4a29524655b538109433b1
Erik Guzman <erik.guzman@guzman.codes>
2020-06-06 18:56:02 -0700

    Create and move resolvers to dedicated files

---
+++ TwitchGraphql/src/resolvers/helix/userResolver.ts
@@ -0,0 +1,37 @@
+import { HelixUser } from "twitch"
+import { RequestContext, ArgumentsWithId, ArgumentsWithName } from '../../interfaces';
+
+export default {
+  HelixUser: {
+    async latestFollowing(parent: HelixUser) {
+      const { data } = await parent.getFollows()
+      return data.map((follow) => ({
+        id: follow.followedUserId,
+        displayName: follow.followedUserDisplayName,
+        date: follow.followDate,
+      }))
+    },
+    async currentStream(parent: HelixUser) {
+      return await parent.getStream()
+    },
+    async clips(parent: HelixUser, _args: any, context: RequestContext) {
+      const { data: clips } = await context.twitchClient.helix.clips.getClipsForBroadcaster(parent.id)
+      return clips
+    },
+    async isFollowingUserId(parent: HelixUser, args: ArgumentsWithId, context: RequestContext) {
+      if(args.userId.length === 0) return false;
+      return await parent.follows(args.userId)
+    },
+    async isFollowingUserName(parent: HelixUser, args: ArgumentsWithName, context: RequestContext) {
+      if(args.userName.length === 0) return false;
+      const user = await context.twitchClient.helix.users.getUserByName(args.userName)
+      return await parent.follows(user!.id)
+    },
+    async videos(parent: HelixUser, args: ArgumentsWithName, context: RequestContext) {
+      return await context.twitchClient.helix.videos.getVideosByUserPaginated(parent).getAll()
+    },
+    async subscribers(parent: HelixUser, args: ArgumentsWithName, context: RequestContext) {
+      return await context.twitchClient.helix.subscriptions.getSubscriptionsPaginated(parent).getAll();
+    },
+  },
+}
--- TwitchGraphql/src/resolvers/helixResolver.ts
+++ TwitchGraphql/src/resolvers/helixResolver.ts
@@ -1,6 +1,6 @@
 import * as helixUsers from '../helixUsers';
-import { HelixUser, HelixStream, HelixStreamType } from 'twitch';
-import { RequestContext, ArgumentsWithIds, ArgumentsWithNames, ArgumentsWithId, ArgumentsWithName } from '../interfaces';
+import { HelixStreamType } from 'twitch';
+import { RequestContext, ArgumentsWithIds, ArgumentsWithNames } from '../interfaces';
 
 export default {
   Query: {
@@ -25,41 +25,4 @@ export default {
       };
     },
   },
-  HelixUser: {
-    async latestFollowing(parent: HelixUser) {
-      const { data } = await parent.getFollows()
-      return data.map((follow) => ({
-        id: follow.followedUserId,
-        displayName: follow.followedUserDisplayName,
-        date: follow.followDate,
-      }))
-    },
-    async currentStream(parent: HelixUser) {
-      return await parent.getStream()
-    },
-    async clips(parent: HelixUser, _args: any, context: RequestContext) {
-      const { data: clips } = await context.twitchClient.helix.clips.getClipsForBroadcaster(parent.id)
-      return clips
-    },
-    async isFollowingUserId(parent: HelixUser, args: ArgumentsWithId, context: RequestContext) {
-      if(args.userId.length === 0) return false;
-      return await parent.follows(args.userId)
-    },
-    async isFollowingUserName(parent: HelixUser, args: ArgumentsWithName, context: RequestContext) {
-      if(args.userName.length === 0) return false;
-      const user = await context.twitchClient.helix.users.getUserByName(args.userName)
-      return await parent.follows(user!.id)
-    },
-    async videos(parent: HelixUser, args: ArgumentsWithName, context: RequestContext) {
-      return await context.twitchClient.helix.videos.getVideosByUserPaginated(parent).getAll()
-    }
-  },
-  HelixStream: {
-    async game(parent: HelixStream) {
-      return await parent.getGame()
-    },
-    async user(parent: HelixStream) {
-      return await parent.getUser()
-    },
-  }
 };
