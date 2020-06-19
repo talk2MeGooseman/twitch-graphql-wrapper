7789cd1eae7e9fca1a8a56f28cb2da42d1b4645a
Erik Guzman <erik.guzman@guzman.codes>
2020-06-19 11:35:16 -0700

    Change express server export name

--- src/express-server/index.ts
+++ src/express-server/index.ts
@@ -3,12 +3,10 @@ import schema from '../schema'
 import depthLimit from 'graphql-depth-limit'
 import context from "../context";
 
-const server = (overrideContext = context) => {
+export const createServer = (overrideContext = context) => {
     new ApolloServer({
     schema,
     validationRules: [depthLimit(7)],
     context: overrideContext,
   });
 };
-
-export default server
