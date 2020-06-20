64b1eb76f3bba225cf0fc18b465e7d69411d9cbc
Erik Guzman <erik.guzman@guzman.codes>
2020-06-19 22:23:53 -0700

    add dotenv for load env vars

--- credentials/twitch.ts
+++ credentials/twitch.ts
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 export default {
   clientId: process.env.TWITCH_CLIENT_ID || "",
   clientSecret: process.env.TWITCH_SECRET || "",
   accessToken: process.env.ACCESS_TOKEN || "",
   refreshToken: process.env.REFRESH_TOKEN || "",
-};
+}
