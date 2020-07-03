e20d63b57768a75719b6531e23dd3beee2209920
Erik Guzman <erik.guzman@guzman.codes>
2020-07-03 12:33:45 -0700

    Add me type to helix

--- src/types/helixType.ts
+++ src/types/helixType.ts
@@ -1,9 +1,15 @@
 const typeDefs = `
   type Helix {
-    usersByIds(ids: [ID!]!): [HelixUser]
-    usersByNames(names: [String!]!): [HelixUser]
+    "Retrieves the user data of the currently authenticated user."
+    me: HelixUser
+    "Retrieves an array of live stream data based off IDs"
     streamsByIds(ids: [ID!]!): [HelixStream]!
+    "Retrieves an array of live stream data based off usernames"
     streamsByNames(names: [String]!): [HelixStream]!
+    "Retrieves an array of user data based off ids"
+    usersByIds(ids: [ID!]!): [HelixUser]
+    "Retrieves an array of user data based off names"
+    usersByNames(names: [String!]!): [HelixUser]
   }
 `
 
